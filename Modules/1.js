79b8b12587f2846e449562c11c10cc0b88f190e3
KazuhikoKuroda <kuroda2395@gmail.com>
2015-11-17 17:52:27 +0900

    Modules 1

---
+++ Modules/1.js
@@ -0,0 +1,10 @@
+var simpleController = function($scope) {
+	$scope.person = {
+		firstName: "kazuhiko",
+		lastName: "kuroda",
+		getFullName: function() {
+			return $scope.person.firstName + " " + $scope.person.lastName;
+		}
+	};
+};
+angular.module("mainModule", []).controller("simpleController", simpleController);
